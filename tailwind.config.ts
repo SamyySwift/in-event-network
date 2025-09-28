cfee2e0b8a4cbe645593067507b6b9f25f3fda88
SamSwift <samueludofia94@gmail.com>
2025-09-28 18:41:28 +0100

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -177,5 +177,8 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("tailwind-scrollbar"),
+  ],
 } satisfies Config;
